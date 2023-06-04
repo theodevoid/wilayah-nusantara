dd4b72750a47a81ad3f95aeb43c16c2e511e48f6
theodevoid <87703322+theodevoid@users.noreply.github.com>
2023-06-04 17:43:46 +0700

    features docs

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,8 +1,32 @@
 export default {
   logo: <strong>Wilayah Nusantara</strong>,
   project: {
-    link: 'https://github.com/theodevoid/wilayah-nusantara'
+    link: 'https://github.com/theodevoid/wilayah-nusantara',
   },
-  // primaryHue: 350
+  // primaryHue: 0,
   // ...
-}
+  head: () => {
+    return (
+      <>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </>
+    );
+  },
+};

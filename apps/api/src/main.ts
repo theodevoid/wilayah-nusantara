ca03c0f81fc9e65e6d1639db8d55d5e182c078b8
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-11-12 14:24:11 +0700

    enable cors localhost 3000

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from '~/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(1945);
 }
